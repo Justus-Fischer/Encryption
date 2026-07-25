6cdb19d2a4b203d086cac6f0523f7dc984544191
justus-fischer <266495557+Justus-Fischer@users.noreply.github.com>
2026-07-25 22:25:57 +0200

    added no files found error

--- FileEncrypter/FileEncrypter.py
+++ FileEncrypter/FileEncrypter.py
@@ -9,20 +9,26 @@ print("")
 print(" ")
 
 
-currentpath = os.path.dirname(os.path.abspath(__file__))
+while True:
 
+    currentpath = os.path.dirname(os.path.abspath(__file__))
 
-allentries = os.listdir(currentpath)
+    allentries = os.listdir(currentpath)
 
+    files = [i for i in allentries if os.path.isfile(os.path.join(currentpath, i))]
 
-files = [i for i in allentries if os.path.isfile(os.path.join(currentpath, i))]
+    print("Welcome to FileEncrypter!")
+    print("Currently active in " + currentpath)
+    print("")
+    print("The FileEncrypter found the following files in the current directory:")
+    print("")
+    can = 0
+    for i in files:
+        if i != "FileEncrypter.py":
+            print(i)
+            can = can + 1
+    if can == 0:
+        print("No files found.")
+        break
 
-print("Welcome to FileEncrypter!")
-print("Currently active in " + currentpath)
-print("")
-print("The FileEncrypter found the following files in the current directory:")
-print("")
-for i in files:
-    print(i)
-
-print("")
+    print("")
