68274e4f4fcff67dff59616172488718ff292894
justus-fischer <266495557+Justus-Fischer@users.noreply.github.com>
2026-07-25 22:49:53 +0200

    added seccond exit option

--- FileEncrypter/FileEncrypter.py
+++ FileEncrypter/FileEncrypter.py
@@ -6,16 +6,21 @@ import os
 print("JUST A DEMO - DO NOT USE FOR REAL SENSITIVE DATA!")
 print("It is possible that data cannot be decrypted")
 print("")
+print("It is recommended to execute this script in a separate folder")
 print(" ")
-
+print("Do you want to continue or exit?")
+choice = input("(continue/exit): ").lower()
 
 while True:
+    if "ex" in choice:
+        print("Have a nice day!")
+        break
 
     currentpath = os.path.dirname(os.path.abspath(__file__))
 
     allentries = os.listdir(currentpath)
 
-    files = [i for i in allentries if os.path.isfile(os.path.join(currentpath, i))]
+    files = files = [i for i in allentries if os.path.isfile(os.path.join(currentpath, i)) and i != "FileEncrypter.py"]
 
     print("Welcome to FileEncrypter!")
     print("Currently active in " + currentpath)
@@ -24,9 +29,10 @@ while True:
     print("")
     can = 0
     for i in files:
-        if i != "FileEncrypter.py":
-            print(i)
-            can = can + 1
+
+        print(i)
+        can = can + 1
+
     if can == 0:
         print("No files found.")
         break
